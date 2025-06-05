9247bf89e375b29e080412944efc336d2b7bb907
jenri019 <j.enrique209436@hotmail.com>
2025-06-05 15:51:17 -0600

    feat: configuring socket server

--- classes/server.ts
+++ classes/server.ts
@@ -1,16 +1,29 @@
 import express from 'express';
 import { SERVER_PORT } from '../global/environment';
+import { Server as socketIO } from "socket.io";
+import http from 'http';
 
 export default class Server {
     public app: express.Application;
     public port: number;
+    public io: socketIO;
+    private httpServer: http.Server;
 
     constructor() {
         this.app = express();
         this.port = SERVER_PORT;
+        this.httpServer = new http.Server(this.app);
+        this.io = new socketIO(this.httpServer); // <-- así se instancia
+    }
+
+    private listenSockets() {
+        console.log('Listening for socket connections');
+        this.io.on('connection', (client) => {
+            console.log('Client connected');
+        });
     }
 
     start(callback: () => void): void {
-        this.app.listen(this.port, callback);
+        this.httpServer.listen(this.port, callback);
     }
 }
--- dist/classes/server.js
+++ dist/classes/server.js
@@ -5,13 +5,23 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const environment_1 = require("../global/environment");
+const socket_io_1 = require("socket.io");
+const http_1 = __importDefault(require("http"));
 class Server {
     constructor() {
         this.app = (0, express_1.default)();
         this.port = environment_1.SERVER_PORT;
+        this.httpServer = new http_1.default.Server(this.app);
+        this.io = new socket_io_1.Server(this.httpServer); // <-- así se instancia
+    }
+    listenSockets() {
+        console.log('Listening for socket connections');
+        this.io.on('connection', (client) => {
+            console.log('Client connected');
+        });
     }
     start(callback) {
-        this.app.listen(this.port, callback);
+        this.httpServer.listen(this.port, callback);
     }
 }
 exports.default = Server;
