a721c26c82c07b9eb228a88915852f034986db39
jenri019 <j.enrique209436@hotmail.com>
2025-06-06 09:38:30 -0600

    feat: moving socket logic to another file

--- classes/server.ts
+++ classes/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { SERVER_PORT } from '../global/environment';
 import { Server as socketIO } from "socket.io";
 import http from 'http';
+import * as socket from '../sockets/socket';
 
 export default class Server {
     private static _instance: Server;
@@ -15,6 +16,7 @@ export default class Server {
         this.port = SERVER_PORT;
         this.httpServer = new http.Server(this.app);
         this.io = new socketIO(this.httpServer); // <-- así se instancia
+        this.listenSockets();
     }
 
     public static get instance() {
@@ -25,6 +27,7 @@ export default class Server {
         console.log('Listening for socket connections');
         this.io.on('connection', (client) => {
             console.log('Client connected');
+            socket.disconnect(client);
         });
     }
 
